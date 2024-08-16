3c5b109b901a831930c15fb1741bf490d0160009
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-08-16 21:29:06 +0600

    added filter section

--- src/Components/AllProducts.jsx
+++ src/Components/AllProducts.jsx
@@ -1,4 +1,225 @@
+// import "../CSS/AllProduct.css";
+// import { useEffect, useState } from "react";
+// import { Link } from "react-router-dom";
 
+// const AllProducts = () => {
+//   const [products, setProducts] = useState([]);
+//   const [count, setCount] = useState(0);
+//   const [itemPerPage, setItemPerPage] = useState(10);
+//   const [currentPage, setCurrentPage] = useState(0);
+//   const [asc, setAsc] = useState(true);
+//   const [search, setSearch] = useState("");
+
+//   useEffect(() => {
+//     fetch(
+//       `http://localhost:5000/allproducts?page=${currentPage}&size=${itemPerPage}&sort=${
+//         asc ? "asc" : "desc"
+//       }&search=${search}`
+//     )
+//       .then((res) => res.json())
+//       .then((data) => setProducts(data));
+//   }, [currentPage, itemPerPage, asc, search]);
+
+//   useEffect(() => {
+//     fetch("http://localhost:5000/productCount")
+//       .then((res) => res.json())
+//       .then((data) => setCount(data.count || 0));
+//   }, []);
+
+//   const numberOfPage = Math.ceil(count / itemPerPage) || 1;
+//   const pages = [...Array(numberOfPage).keys()];
+
+//   const handelItemPerPage = (e) => {
+//     const val = parseInt(e.target.value);
+//     setItemPerPage(val);
+//     setCurrentPage(0);
+//   };
+
+//   const handelPreviousPage = () => {
+//     if (currentPage > 0) {
+//       setCurrentPage(currentPage - 1);
+//     }
+//   };
+
+//   const handelNextPage = () => {
+//     if (currentPage < pages.length - 1) {
+//       setCurrentPage(currentPage + 1);
+//     }
+//   };
+
+//   const handelSearch = (e) => {
+//     e.preventDefault();
+//     const searchText = e.target.search.value;
+//     setSearch(searchText);
+//   };
+
+//   return (
+//     <div className="container mx-auto px-3">
+//       <h1 className="my-10 text-center font-bold lg:text-5xl md:text-4xl text-white">
+//         Find Your Dream Products{" "}
+//       </h1>
+
+//       <div className="mb-10 flex gap-6">
+//         {/* price asc desc button */}
+//         <div>
+//           <button
+//             className="btn text-[16px] font-bold"
+//             onClick={() => setAsc(!asc)}
+//           >
+//             {asc ? "Price: High to Low" : "Price: Low to High"}
+//           </button>
+//         </div>
+//         {/* search field */}
+//         <div>
+//           <form className="flex" onSubmit={handelSearch}>
+//             <input
+//               type="text"
+//               name="search"
+//               placeholder="product name or Category"
+//               className="input input-bordered w-full max-w-xs"
+//             />
+//             <input type="submit" value="Search" className="btn" />
+//           </form>
+//         </div>
+//       </div>
+
+//       <div className="flex gap-4 text-black mb-10">
+//         {/* brand name */}
+//         <div>
+//           <label className="text-white">Brand Name</label>
+//           <select
+//             id="carCondition"
+//             name="carCondition"
+//             className="text-lightBlack border-[1px] border-gray px-2 py-1 rounded focus:outline-none "
+//           >
+//             <option disabled value="">
+//               Choose Your Brand
+//             </option>
+//             <option value="all">All</option>
+//             <option value="Apple">apple</option>
+//             <option value="Ring">ring</option>
+//             <option value="logitech">logitech</option>
+//             <option value="sony">sony</option>
+//             <option value="hamilton">hamilton</option>
+//             <option value="gopro">gopro</option>
+//             <option value="corsair">corsair</option>
+//             <option value="anker">anker</option>
+//           </select>
+//         </div>
+
+//         {/* category name */}
+//         <div>
+//           <label className="text-white">Category Name</label>
+//           <select
+//             id="carCondition"
+//             name="carCondition"
+//             className="text-lightBlack border-[1px] border-gray px-2 py-1 rounded focus:outline-none"
+//           >
+//             <option disabled value="">
+//               Choose car category
+//             </option>
+//             <option value="all">All</option>
+//             <option value="Electronics">Electronics</option>
+//             <option value="Accessories">Accessories</option>
+//             <option value="Wearables">Wearables</option>
+//             <option value="Home Automation">Home Automation</option>
+//             <option value="Kitchen Appliances">Kitchen Appliances</option>
+//             <option value="Personal Care">Personal Care</option>
+//             <option value="Wearables">Wearables</option>
+//             <option value="Home Automation">Home Automation</option>
+//             <option value="Kitchen Appliances">Kitchen Appliances</option>
+//           </select>
+//         </div>
+
+//         {/* price range */}
+//         <div>
+//           <label className="text-white">Price Range</label>
+//           <select
+//             id="carCondition"
+//             name="carCondition"
+//             className="text-lightBlack border-[1px] border-gray px-2 py-1 rounded focus:outline-none"
+//           >
+//             <option disabled value="">
+//               Choose price range
+//             </option>
+//             <option value="all">All</option>
+//             <option value="$10-$50">$10-$50</option>
+//             <option value="$50-$100">$50-$100</option>
+//             <option value="$100-$200">$100-$200</option>
+//           </select>
+//         </div>
+//       </div>
+
+//       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
+//         {products.map((product) => (
+//           <div
+//             key={product._id}
+//             className="card card-compact bg-[#ecedf3] h-96 shadow-xl"
+//           >
+//             <figure>
+//               <img
+//                 className="w-full"
+//                 src={product.product_image}
+//                 alt={product.name}
+//               />
+//             </figure>
+//             <div className="card-body">
+//               <h2 className="card-title">{product.product_name}</h2>
+//               <div className="flex justify-around items-center">
+//                 <p>
+//                   <span className="text-[16px] font-bold">Date:</span>{" "}
+//                   {product.Date}
+//                 </p>
+//                 <p>
+//                   <span className="text-[16px] font-bold">Rating:</span>{" "}
+//                   {product.Ratings}
+//                 </p>
+//                 <p className="text-[16px] font-bold">${product.Price}</p>
+//               </div>
+
+//               <div className="flex">
+//                 <p>
+//                   <span className="text-[16px] font-bold">Category:</span>{" "}
+//                   {product.Category}
+//                 </p>
+//                 <Link>
+//                   <button className="text-xl text-red-500">view more</button>
+//                 </Link>
+//               </div>
+//             </div>
+//           </div>
+//         ))}
+//       </div>
+
+//       {/* pagination */}
+//       <div className="pagination mt-10 space-y-2">
+//         <button onClick={handelPreviousPage}>Pre</button>
+//         {pages.map((page) => (
+//           <button
+//             className={currentPage === page ? "selected" : undefined}
+//             onClick={() => setCurrentPage(page)}
+//             key={page}
+//           >
+//             {page}
+//           </button>
+//         ))}
+//         <button onClick={handelNextPage}>Next</button>
+//         <select
+//           className="bg-[#0A1316] text-white btn"
+//           value={itemPerPage}
+//           onChange={handelItemPerPage}
+//         >
+//           <option value="5">5</option>
+//           <option value="10">10</option>
+//           <option value="20">20</option>
+//           <option value="30">30</option>
+//         </select>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default AllProducts;
 import "../CSS/AllProduct.css";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
@@ -10,16 +231,21 @@ const AllProducts = () => {
   const [currentPage, setCurrentPage] = useState(0);
   const [asc, setAsc] = useState(true);
   const [search, setSearch] = useState("");
+  const [brand, setBrand] = useState("");
+  const [category, setCategory] = useState("");
+  const [priceRange, setPriceRange] = useState("");
 
   useEffect(() => {
     fetch(
       `http://localhost:5000/allproducts?page=${currentPage}&size=${itemPerPage}&sort=${
         asc ? "asc" : "desc"
-      }&search=${search}`
+      }&search=${search}&brand=${brand}&category=${category}&priceRange=${priceRange}`
     )
       .then((res) => res.json())
       .then((data) => setProducts(data));
-  }, [currentPage, itemPerPage, asc, search]);
+  }, [currentPage, itemPerPage, asc, search, brand, category, priceRange]);
+
+  console.log(products);
 
   useEffect(() => {
     fetch("http://localhost:5000/productCount")
@@ -54,10 +280,14 @@ const AllProducts = () => {
     setSearch(searchText);
   };
 
+  const handleBrandChange = (e) => setBrand(e.target.value);
+  const handleCategoryChange = (e) => setCategory(e.target.value);
+  const handlePriceRangeChange = (e) => setPriceRange(e.target.value);
+
   return (
     <div className="container mx-auto px-3">
       <h1 className="my-10 text-center font-bold lg:text-5xl md:text-4xl text-white">
-        Find Your Dream Products{" "}
+        Find Your Dream Products
       </h1>
 
       <div className="mb-10 flex gap-6">
@@ -81,6 +311,66 @@ const AllProducts = () => {
             />
             <input type="submit" value="Search" className="btn" />
           </form>
+        </div>
+      </div>
+
+      <div className="flex gap-4 text-black mb-10">
+        {/* brand name */}
+        <div>
+        <label className="text-white">Brand Name</label>
+          <select
+            name="brand"
+            className="text-lightBlack border-[1px] border-gray px-2 py-1 rounded focus:outline-none"
+            onChange={handleBrandChange}
+            value={brand}
+          >
+            <option value="">Choose Your Brand</option>
+            <option value="all">All</option>
+            <option value="Apple">Apple</option>
+            <option value="Ring">Ring</option>
+            <option value="logitech">Logitech</option>
+            <option value="sony">Sony</option>
+            <option value="hamilton">Hamilton</option>
+            <option value="gopro">GoPro</option>
+            <option value="corsair">Corsair</option>
+            <option value="anker">Anker</option>
+          </select>
+        </div>
+
+        {/* category name */}
+        <div>
+          <label className="text-white">Category Name</label>
+          <select
+            name="category"
+            className="text-lightBlack border-[1px] border-gray px-2 py-1 rounded focus:outline-none"
+            onChange={handleCategoryChange}
+            value={category}
+          >
+            <option value="">Choose Category</option>
+            <option value="all">All</option>
+            <option value="Electronics">Electronics</option>
+            <option value="Accessories">Accessories</option>
+            <option value="Wearables">Wearables</option>
+            <option value="Home Automation">Home Automation</option>
+            <option value="Kitchen Appliances">Kitchen Appliances</option>
+            <option value="Personal Care">Personal Care</option>
+          </select>
+        </div>
+
+        {/* price range */}
+        <div>
+          <label className="text-white">Price Range</label>
+          <select
+            name="priceRange"
+            className="text-lightBlack border-[1px] border-gray px-2 py-1 rounded focus:outline-none"
+            onChange={handlePriceRangeChange}
+            value={priceRange}
+          >
+            <option value="">Choose Price Range</option>
+            <option value="$10-$50">$10-$50</option>
+            <option value="$50-$100">$50-$100</option>
+            <option value="$100-$200">$100-$200</option>
+          </select>
         </div>
       </div>
 
@@ -110,7 +400,10 @@ const AllProducts = () => {
                 </p>
                 <p className="text-[16px] font-bold">${product.Price}</p>
               </div>
-
+              <p>
+                <span className="text-[16px] font-bold">Brand: </span>
+                {product.Brand}
+              </p>
               <div className="flex">
                 <p>
                   <span className="text-[16px] font-bold">Category:</span>{" "}
